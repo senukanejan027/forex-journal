d0e9d5bd9f6a5c8732db0933a524d1c0a5237667
senukanejan027 <senukanejan027@gmail.com>
2025-12-27 17:31:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function calculateLot() {
+  let balance = document.getElementById("balance").value;
+  let risk = document.getElementById("risk").value;
+  let sl = document.getElementById("sl").value;
+  let pip = document.getElementById("pip").value;
+
+  let lot = (balance * (risk / 100)) / (sl * pip);
+  document.getElementById("result").innerText =
+    "Recommended Lot Size: " + lot.toFixed(2);
+}
 
